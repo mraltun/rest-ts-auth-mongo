f5c46ea3d533efafee7f715865f3eb70140eb571
Murat Altun <mraltun@gmail.com>
2023-07-11 18:04:27 +0300

    Add isAuthenticated middleware

--- src/controllers/authentication.ts
+++ src/controllers/authentication.ts
@@ -1,6 +1,6 @@
 import express from "express";
-import { createUser, getUserByEmail } from "db/users";
-import { random, authentication } from "helpers";
+import { createUser, getUserByEmail } from "../db/users";
+import { random, authentication } from "../helpers";
 
 export const login = async (req: express.Request, res: express.Response) => {
   try {
