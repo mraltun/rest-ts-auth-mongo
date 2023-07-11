82dfa42b41560329138bb17fd7c3dbefd476ae7e
Murat Altun <mraltun@gmail.com>
2023-07-11 18:22:46 +0300

    isOwner middleware

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -2,6 +2,27 @@ import express from "express";
 import { get, merge } from "lodash";
 import { getUserBySessionToken } from "../db/users";
 
+export const isOwner = async (
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) => {
+  try {
+    const { id } = req.params;
+    const currentUserId = get(req, "identity._id") as string;
+
+    if (!currentUserId) {
+      return res.sendStatus(403);
+    }
+
+    if (currentUserId.toString() !== id) {
+      return res.sendStatus(403);
+    }
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(400);
+  }
+};
 export const isAuthenticated = async (
   req: express.Request,
   res: express.Response,
@@ -20,7 +41,7 @@ export const isAuthenticated = async (
       return res.sendStatus(403);
     }
 
-    merge(req, { identitity: existingUser });
+    merge(req, { identity: existingUser });
   } catch (error) {
     console.log(error);
     res.sendStatus(400);
