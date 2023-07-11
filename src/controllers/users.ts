60e482bccb6c73745f9e857ffe8ce3cd17befef8
Murat Altun <mraltun@gmail.com>
2023-07-11 18:27:11 +0300

    updateUser controller

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { deleteUserById, getUsers } from "../db/users";
+import { deleteUserById, getUserbyId, getUsers } from "../db/users";
 
 export const getAllUsers = async (
   req: express.Request,
@@ -20,10 +20,33 @@ export const deleteUser = async (
   res: express.Response
 ) => {
   try {
-    const {id} = req.params
-    const deletedUser = await deleteUserById(id)
+    const { id } = req.params;
+    const deletedUser = await deleteUserById(id);
 
-    return res.json(deletedUser)
+    return res.json(deletedUser);
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+  }
+};
+
+export const updateUser = async (
+  req: express.Request,
+  res: express.Response
+) => {
+  try {
+    const { id } = req.params;
+    const { username } = req.body;
+
+    if (!username) {
+      return res.sendStatus(400);
+    }
+
+    const user = await getUserbyId(id);
+    user.username = username;
+    await user.save();
+
+    return res.status(200).json(user).end();
   } catch (error) {
     console.log(error);
     return res.sendStatus(400);
