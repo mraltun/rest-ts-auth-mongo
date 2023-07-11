2bf4c1e86d04fd424f309abc9d41e9e095254371
Murat Altun <mraltun@gmail.com>
2023-07-11 15:48:31 +0300

    Add Schema and Model for Users

--- src/index.ts
+++ src/index.ts
@@ -4,25 +4,28 @@ import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import compression from "compression";
 import cors from "cors";
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-const app = express()
+const app = express();
 
-app.use(cors({
-    credentials: true
-}))
+app.use(
+  cors({
+    credentials: true,
+  })
+);
 
-app.use(compression())
-app.use(cookieParser())
-app.use(bodyParser.json())
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
 
-const server = http.createServer(app)
-server.listen(8080, ()=>{
-    console.log("Server running on http://localhost:8080")
-})
+const server = http.createServer(app);
+server.listen(8080, () => {
+  console.log("Server running on http://localhost:8080");
+});
 
-const MONGO_URL = "mongodb+srv://mraltun:<password>@cluster0.btiys.mongodb.net/?retryWrites=true&w=majority"
+const MONGO_URL =
+  "mongodb+srv://mraltun:<password>@cluster0.btiys.mongodb.net/?retryWrites=true&w=majority";
 
 mongoose.Promise = Promise;
-mongoose.connect(MONGO_URL)
-mongoose.connection.on("error", (error: Error)=> console.log(error))
+mongoose.connect(MONGO_URL);
+mongoose.connection.on("error", (error: Error) => console.log(error));
