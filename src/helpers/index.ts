7b186c3f7ad130a0705a09b74518ee5c849179aa
Murat Altun <mraltun@gmail.com>
2023-07-12 11:29:00 +0300

    Fix authentication type problem

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -3,10 +3,9 @@ import crypto from "crypto";
 const SECRET = "TEMPSECRET";
 
 export const random = () => crypto.randomBytes(128).toString("base64");
-export const authentication = (salt: string, password: string) => {
+export const authentication = (salt: string, password: string): string => {
   return crypto
     .createHmac("sha256", [salt, password].join("/"))
     .update(SECRET)
-    .digest();
+    .digest("hex");
 };
-
